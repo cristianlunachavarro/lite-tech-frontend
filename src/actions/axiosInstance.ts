4463cf30d735980bba338ff90058b5e9ce8968b8
Cristian Luna <cristianluna@Cristians-MBP-2.fibertel.com.ar>
2024-10-09 17:58:24 -0300

    set env variables

--- src/actions/axiosInstance.ts
+++ src/actions/axiosInstance.ts
@@ -1,8 +1,7 @@
-// src/services/axiosInstance.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
